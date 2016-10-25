1e4d81c8f0c4904b05decdaf1508c51095b5cdca
quynhnguyen895 <quynhnguyen895@Utexas.edu>
2016-10-24 23:28:44 -0500

    added clustering

--- 02Shiny/server.R
+++ 02Shiny/server.R
@@ -1,4 +1,5 @@
 # server.R
+# PROJECT 4
 require("jsonlite")
 require("RCurl")
 require(ggplot2)
@@ -6,13 +7,46 @@ require(dplyr)
 require(shiny)
 
 shinyServer(function(input, output) {
+  x_col_name <- reactive({input$x_col_name})    
+  y_col_name <- reactive({input$y_col_name})
 
-  output$plot <- renderPlot({
-
-  df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'oraclerest.cs.utexas.edu:5001/rest/native/?query=
-  "select * from GASISDATA
-  ')), httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_qmn76', PASS='orcl_qmn76', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE)))
+  # GET DATA
+  df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from GASISDATA"'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_qmn76', PASS='orcl_qmn76', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
   
+  # GET DATA, FILTERED BY SELECTED X_COL_NAME AND Y_COL_NAME
+  selectedXData <- reactive({
+    df[, c(input$x_col_name)]
+  })
+  
+  selectedYData <- reactive({
+    df[, c(input$y_col_name)]
+  })
+  
+  # UPDATE DATA AS NEEDED
+  selectedXandY <- reactive({
+    df[, c(input$x_col_name, input$y_col_name)]
+  })
+  
+  # CLUSTER THE DATA
+  clusters <- reactive({
+    kmeans(selectedXandY(),input$num_of_clusters)
+  })
+
+  # PLOT THE CLUSTERED DATA
+  output$clusterPlot <- renderPlot({
+    plot(selectedXandY(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    
+  })
+  
+  output$xSelector <- renderUI({
+    sliderInput("RangeX", 
+                "Min and Max of X Variable:", 
+                min = min(selectedXData()),
+                max = max(selectedXData()), 
+                value = mean(selectedXData(),na.rm = TRUE))
+    })
   
   })
-})
--- 02Shiny/ui.R
+++ 02Shiny/ui.R
@@ -1,4 +1,5 @@
 #ui.R 
+# PROJECT 4
 
 library(shiny)
 require(shinydashboard)
@@ -10,21 +11,32 @@ headerPanel("Hello Shiny!"),
 
 # Sidebar with a slider input for number of observations
   sidebarPanel(
-    sliderInput("KPI1", 
-                "KPI_Low_Max_value:", 
+    
+    # Dropdown to decide which colum to plot the histogram of 
+    selectInput('hist_col_name', 'Variable To Plot Histogram Of', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="RESTEMP"),
+
+    # Dropdown to decide which colum to plot the k-means of _
+    selectInput('x_col_name', 'X Variable', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="AVPOR"),
+    selectInput('y_col_name', 'Y Variable', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="AVPERM"),
+
+    #uiOutput("xSelector"),
+    
+    sliderInput("RangeX", 
+                "Min and Max of X Variable:", 
                 min = 1,
                 max = 4750, 
                 value = 4750),
-    sliderInput("KPI2", 
-                "KPI_Medium_Max_value:", 
+    sliderInput("RangeY", 
+                "Min and Max of Y Variable:", 
                 min = 4750,
                 max = 5000, 
-                value = 5000)
-  ),
+                value = 5000),
+    
+    numericInput("num_of_clusters", label = h3("Enter The Amount of Clusters"), value = 3)),
 
 # Show a plot of the generated distribution
   mainPanel(
-    #plotOutput("distPlot")
+    plotOutput("clusterPlot")
     #plotOutput("distTable")
   )
 ))
