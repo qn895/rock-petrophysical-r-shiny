27b43256c42c0ef4e3766af689619348c8cc4d75
quynhnguyen895 <quynhnguyen895@Utexas.edu>
2016-10-26 16:04:31 -0500

    data dict + fix some issues
    added data key, fixed some issues, removed main filtering options

--- 02Shiny/server.R
+++ 02Shiny/server.R
@@ -7,13 +7,56 @@ require(dplyr)
 require(shiny)
 
 shinyServer(function(input, output) {
+  # CREATE A DICTIONARY FOR COLUMN NAMES
+  key = c("STATE",
+          "FLDNAME",
+          "RESNAME",
+          "R_STUDY",
+          "PLAYNAME",
+          "PLAYCOD",
+          "ERANM",
+          "SUBPLAYN",
+          "SYSNM",
+          "SERNM",
+          "S_POR",
+          "S_PERM",
+          "LINKA",
+          "AVPOR",
+          "LOPOR",
+          "HIPOR",
+          "AVPERM",
+          "LOPERM",
+          "HIPERM",
+          "RESTEMP") 
+  
+  desc = c("State",
+           "Doe/Eia Field Name",
+           "Reservoir Name",
+           "Reservoir Source (Atlas, Reserv. Study, Totl)",
+           "Gas Atlas Geologic Play Name",
+           "Gas Atlas Geologic Play Code",
+           "Geologic Era Name",
+           "Gas Atlas Subplay Name",
+           "Geologic System Name",
+           "Geologic Series Name",
+           "Data Source For Porosity",
+           "Data Source For Permeability",
+           "Unique Key For Reservoir Data System",
+           "Average Porosity Of Pay Interval",
+           "Porosity Range Minimum -Net Pay",
+           "Porosity Range Maximum -Net Pay",
+           "Average Permeability Of Pay Interval",
+           "Permeability Range Minimum",
+           "Permeability Range Maximum",
+           "Average Reservoir Temperature")
+  col_dictionary <- as.list(desc)
+  names(col_dictionary) <- key
+  
+  # PROCESSING INPUT VARIABLES
   hist_col_name <- reactive({input$hist_col_name})
   x_col_name <- reactive({input$x_col_name})    
   y_col_name <- reactive({input$y_col_name})
 
-  # GET DATA
-  # states = c('TEXAS', 'ALABAMA')
-  # state_query = paste("where state in (",paste(states, collapse=", "),")")
   df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from GASISDATA"'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_qmn76', PASS='orcl_qmn76', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
   
   subset(df)
@@ -45,20 +88,26 @@ shinyServer(function(input, output) {
   # PLOT THE CLUSTERED DATA
   output$clusterPlot <- renderPlot({
     plot(selectedXandY(),
+         xlab = col_dictionary[[input$x_col_name]],
+         ylab = col_dictionary[[input$y_col_name]],
+         main = "K-means Clustering",
          col = clusters()$cluster,
          pch = 20, cex = 3)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
     
   })
   
+  # PLOT THE HISTOGRAM WHERE THE COLUMN AND THE NUMBER OF BINS IS REACTIVE
   output$histPlot <- renderPlot({
     hist(selectedHistData(),
         probability = FALSE,
         breaks = as.numeric(input$num_of_bins),
-        main = "Frequency")
+        main = paste("Frequency (or Count) of", col_dictionary[[input$hist_col_name]]),
+        xlab=col_dictionary[[input$hist_col_name]])
   })
 
-  output$campSelector <- renderUI({
+  # CREATE A SHINY UI ELEMENT THAT LISTS THE STATES FOR THE HISTOGRAM
+  output$stateSelector <- renderUI({
     states <- unique(df['STATE'])
     selectInput("state", "Choose State To Filter Data By:", as.list(states),selected="TEXAS") 
   })
@@ -76,7 +125,8 @@ shinyServer(function(input, output) {
   output$boxPlot <- renderPlot({
     boxplot(as.formula(formulaText()), 
             data = df,
-            outline = input$outliers)
+            outline = input$outliers,
+            main = paste("Box Plot of", col_dictionary[[input$variable]]))
   })
   
   })
--- 02Shiny/ui.R
+++ 02Shiny/ui.R
@@ -14,13 +14,31 @@ headerPanel("Project 4!"),
 
     # Dropdown to decide which colum to plot the histogram of 
     h3("Histogram Options"),
-    selectInput('hist_col_name', 'Variable To Plot Histogram Of', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="RESTEMP"),
+    selectInput('hist_col_name', 'Variable To Plot Histogram Of', list("Average Porosity Of Pay Interval" = "AVPOR",
+                                                                       "Porosity Range Minimum -Net Pay" = "LOPOR",
+                                                                       "Porosity Range Maximum -Net Pay" = "HIPOR",
+                                                                       "Average Permeability Of Pay Interval" = "AVPERM",
+                                                                       "Permeability Range Minimum" = "LOPERM",
+                                                                       "Permeability Range Maximum" = "HIPERM",
+                                                                       "Average Reservoir Temperature" = "RESTEMP"),selected="RESTEMP"),
     numericInput("num_of_bins", label = ("Enter The Number of Cells"), value = 20),
     
     # Dropdown to decide which colum to plot the k-means of _
     h3("K-means Clustering Options"),
-    selectInput('x_col_name', 'X Variable', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="AVPOR"),
-    selectInput('y_col_name', 'Y Variable', list("AVPOR","LOPOR","HIPOR","AVPERM","LOPERM","HIPERM","RESTEMP"),selected="AVPERM"),
+    selectInput('x_col_name', 'X Variable', list("Average Porosity Of Pay Interval" = "AVPOR",
+                                                 "Porosity Range Minimum -Net Pay" = "LOPOR",
+                                                 "Porosity Range Maximum -Net Pay" = "HIPOR",
+                                                 "Average Permeability Of Pay Interval" = "AVPERM",
+                                                 "Permeability Range Minimum" = "LOPERM",
+                                                 "Permeability Range Maximum" = "HIPERM",
+                                                 "Average Reservoir Temperature" = "RESTEMP"),selected="AVPOR"),
+    selectInput('y_col_name', 'Y Variable', list("Average Porosity Of Pay Interval" = "AVPOR",
+                                                 "Porosity Range Minimum -Net Pay" = "LOPOR",
+                                                 "Porosity Range Maximum -Net Pay" = "HIPOR",
+                                                 "Average Permeability Of Pay Interval" = "AVPERM",
+                                                 "Permeability Range Minimum" = "LOPERM",
+                                                 "Permeability Range Maximum" = "HIPERM",
+                                                 "Average Reservoir Temperature" = "RESTEMP"),selected="AVPERM"),
 
     numericInput("num_of_clusters", label = ("Enter The Amount of Clusters"), value = 3),
 
@@ -30,20 +48,7 @@ headerPanel("Project 4!"),
                      "Geologic Era" = "ERANM", 
                      "Geologic System Name" = "SYSNM")),
     
-    checkboxInput("outliers", "Show outliers", FALSE),
-    
-    h3("Main Filtering Options"),
-    sliderInput("range_of_x_variable", 
-                label = ("Min and Max of X Variable"), 
-                min = 0, 
-                max = 100, 
-                value = c(40, 60)),
-    sliderInput("range_of_y_variable", 
-                label = ("Min and Max of Y Variable"), 
-                min = 0, 
-                max = 100, 
-                value = c(40, 60)),
-    actionButton(inputId="updateFilters", label="Update Filters")
+    checkboxInput("outliers", "Show outliers", FALSE)
   
 ),
 
